3bf7cb169b95bc8f4c8c7af3ba202bbaea60ffd4
Radowids <radekgil9@o2.pl>
2025-02-13 12:15:57 +0100

    testUpdate method added to UpdateTesting

--- src/main/java/pl/programujodpodstaw/UpdateTesting.java
+++ src/main/java/pl/programujodpodstaw/UpdateTesting.java
@@ -3,6 +3,14 @@ package pl.programujodpodstaw;
 public class UpdateTesting {
     boolean isOk;
     int numberOfUpdates;
+    String updateName;
     String errorCode;
 
+    public String testUpdate (boolean isOk){
+        if (isOk){
+            return updateName;
+        }else{
+            return errorCode;
+        }
+    }
 }
